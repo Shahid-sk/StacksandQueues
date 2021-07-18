4ccb70d16279b748a703dc17fe09582d6378c50f
Shahid-sk <mail2sk.shahid@gmail.com>
2021-07-18 19:05:38 +0530

    added: created a queue

---
+++ src/queue/QueueMain.java
@@ -0,0 +1,14 @@
+package queue;
+
+public class QueueMain {
+	public static void main(String args[]) {
+		Queue<Integer> newQueue = new Queue<>();
+		newQueue.enQueue(56);
+		newQueue.enQueue(30);
+		newQueue.enQueue(70);
+
+		// print queue
+		newQueue.printQueue();
+	}
+
+}
